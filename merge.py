0251c294878026fc47833d35945023de277fe201
Rakhi Agrawal <rakhi@Rakhis-MacBook-Air.local>
2020-09-15 10:50:38 -0500

    fixing kernel issues

--- merge.py
+++ merge.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 csv_names = ['CPI_3.8_Abuse_Neglect_Investigations_-_Victims_with_Demographics_by_Region_FY2010-FY2019', 
             'CPS_2.1_Removals_-_by_Region_with_Child_Demographics_FY2010-2019', 
-            'CPS_3.1_Placement_Types_of_Children_in_Substitute_Care_During_the_Fiscal_Year_by_County_with_Demographics_FY2010-2019', 
-            'CPS_3.2_in_Substitute_Care_on_August_31_by_Placement_Type_with_Demographics_FY2010-2019', 
-            'CPS_4.1_Adoption_-_Children_Waiting_For_Adoption_on_31_August_by_Region_with_Demographics_FY2010-2019', 
-            'CPS_4.3_Adoption_-_Children_In_Adoption_Placements_by_Region_with_Demographics_FY2010-2019', 
-            'CPS_4.4_Adoption_-_Disabling_Conditions_of_Children_in_Adoption_Placements_by_Region_with_Demographics_FY2010-2019', 
-            'CPS_4.5_Adoptions_Consummated_by_Region_with_Demographics_FY2010-2019']
+            'CPS_3.1_Placement_Types_of_Children_in_Substitute_Care_During_the_Fiscal_Year_by_County_with_Demographics_FY2010-2019']
+            # 'CPS_3.2_in_Substitute_Care_on_August_31_by_Placement_Type_with_Demographics_FY2010-2019', 
+            # 'CPS_4.1_Adoption_-_Children_Waiting_For_Adoption_on_31_August_by_Region_with_Demographics_FY2010-2019', 
+            # 'CPS_4.3_Adoption_-_Children_In_Adoption_Placements_by_Region_with_Demographics_FY2010-2019', 
+            # 'CPS_4.4_Adoption_-_Disabling_Conditions_of_Children_in_Adoption_Placements_by_Region_with_Demographics_FY2010-2019', 
+            # 'CPS_4.5_Adoptions_Consummated_by_Region_with_Demographics_FY2010-2019']
 
 dfs = {}
 for i, csv in enumerate(csv_names):
@@ -24,19 +24,17 @@ for i, csv in enumerate(csv_names):
 df0 = dfs[0]
 df1 = dfs[1]
 df2 = dfs[2]
-df3 = dfs[3]
-df4 = dfs[4]
-df5 = dfs[5]
-df6 = dfs[6]
-df7 = dfs[7]
+# df3 = dfs[3]
+# df4 = dfs[4]
+# df5 = dfs[5]
+# df6 = dfs[6]
+# df7 = dfs[7]
 # %%
-final_df = df2.copy()
+# final_df = df2.copy()
 # %%
 final_df.info()
 # %%
 final_df.drop(['County'], axis=1, inplace=True)
-# %%
-final_df
 # %%
 # bucketize the 'Age' column into 'Age Group'
 
@@ -46,12 +44,12 @@ final_df['Age Group'] = pd.cut(final_df['Age'], bins, labels=labels)
 final_df.drop(['Age'], axis=1, inplace=True)
 
 # %%
-final_df
-# %%
 type_subs_care_count = final_df.groupby(['Fiscal Year','Region', 'Gender', 'Race/Ethnicity', 'Age Group', 'Type of Substitute Care']).count()['Placed with Relative']
-pd.pivot_table(type_subs_care_count, values=type_subs_care_count['Type of Substitute Care'], index = ['Fiscal Year', 'Region', 'Gender', 'Race/Ethnicity', 'Age Group'])
+# pd.pivot_table(type_subs_care_count, values=type_subs_care_count['Type of Substitute Care'], index = ['Fiscal Year', 'Region', 'Gender', 'Race/Ethnicity', 'Age Group'])
 df_new = pd.pivot_table(type_subs_care_count, values='Type of Substitute Care', index = ['Fiscal Year', 'Region', 'Gender', 'Race/Ethnicity', 'Age Group'])
-df_new
+df_new.head()
+# %%
+pd.pivot_table(final_df, values='Placed with Relative', index = ['Fiscal Year', 'Region', 'Gender', 'Race/Ethnicity', 'Age Group'], columns=['Type of Substitute Care'], aggfunc='count')
 # %%
 type_subs_care_count.reset_index()
 # %%
